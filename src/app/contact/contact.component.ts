834d1b357ce938028af11c8fad0abc125de564e6
alikaganbertizlioglu <alikaganbertizlioglu@gmail.com>
2023-08-15 13:37:13 +0300

    Contact form's API connection established and tested

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-contact',
@@ -7,22 +8,26 @@ import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms'
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent implements OnInit {
-  form!: FormGroup;
 
+  readonly APIUrl = "https://localhost:7083";
   FormData!: FormGroup;
-  constructor(private builder: FormBuilder) { }
+
+  constructor(private builder: FormBuilder, private http: HttpClient) { }
 
   ngOnInit() {
     this.FormData = this.builder.group({
       Fullname: new FormControl('', [Validators.required]),
       Email: new FormControl('', [Validators.required, Validators.email]),
       Comment: new FormControl('', [Validators.required])
-    })
+    });
   }
 
-  onSubmit(FormData: any) {
+  onSubmit() {
+    console.log(this.FormData.value);
     
+
+    this.http.post(this.APIUrl + '/addContactForm', this.FormData.value).subscribe(data => {
+      alert(data);
+    });
   }
-    
 }
-
