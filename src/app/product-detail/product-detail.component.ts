1dd879c61b4a4262a4ef5fdc51d692c384131b4c
alikaganbertizlioglu <alikaganbertizlioglu@gmail.com>
2023-08-18 08:40:55 +0300

    Minor CSS changes done and product detail page added

--- src/app/product-detail/product-detail.component.ts
+++ src/app/product-detail/product-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Product } from '../Models/Product';
 
 @Component({
@@ -8,5 +8,10 @@ import { Product } from '../Models/Product';
 })
 export class ProductDetailComponent {
 
+  @Input()
   product:Product;
+
+  
+
+
 }
